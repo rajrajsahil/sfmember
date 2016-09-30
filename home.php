8841b5693a912351caedb5e1fe64945cfabef357
Esha Lath <elath03@gmail.com>
2016-09-30 14:25:40 +0530

    home page for head and subhead

--- home.php
+++ home.php
@@ -1,17 +1,15 @@
 
 <?php
-include_once "connect.php";
+require_once "connect.php";
 if(!$user->is_loggedin())
 {
  $user->redirect('index.php');
 }
 $name = $_SESSION['user_session'];
-$stmt = $conn->prepare("SELECT * FROM membercredentials WHERE name=:name");
+$stmt = $conn->prepare("SELECT * FROM login_detail WHERE username=:name");
 $stmt->execute(array(":name"=>$name));
 $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
-$name = $userRow['name'];
-//echo $userRow['name'];
-//include "subhead.php";
+$name = $userRow['username'];
 ?>
 <!DOCTYPE html>
 <html>
@@ -21,14 +19,14 @@ $name = $userRow['name'];
 <body>
 <h3><?php echo $name;
 ?></h3>
+ <label><a href="logout.php?logout=true"><i class="glyphicon glyphicon-log-out"></i> logout</a></label>
 </body>
 </html>
 <?php
 if($userRow['designation']=='head'){
     include "head.php";
 }
-else{
-    include "subhead.php";
-}
+if($userRow['designation']=='subhead'){
+ include "subhead.php";}
 ?>
 
